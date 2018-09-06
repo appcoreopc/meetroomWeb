0b7aaef51fb11cda0daec1f4e72bffb51c146ad6
jeremy woo <jeremy.woo@akqa.com>
2018-09-07 07:23:12 +1200

    added react-redux, react-saga and antd into project.

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,25 @@ import TopNavigation from './components/topNavigation/topNavigation';
 import TopTiles from './components/topNavigation/topTiles';
 import MainContent from './components/content/mainContent';
 import TransactionSummary from './components/content/transactionSummary';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
+const store = createStore(rootReducer) 
 
 ReactDOM.render(
-   
+       
+    <Provider store={store}>
+    
     <div>
-         <AdminTab />
-         <Footer />
-         <TopNavigation />
-         <TopTiles />    
-         <MainContent />  
-         <TransactionSummary />
-            
-     </div>   
-   ,
-    document.getElementById("example")
+        <AdminTab />
+        <Footer />
+        <TopNavigation />
+        <TopTiles />    
+        <MainContent />  
+        <TransactionSummary />    
+    </div>   
+    </Provider>,
+    document.getElementById("root")
 );
