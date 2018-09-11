11e44df7412002325b611ee20788af5c7389bfab
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2018-09-11 22:09:12 +1200

    added table.

--- src/components/content/mainContent.tsx
+++ src/components/content/mainContent.tsx
@@ -1,48 +1,66 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { Table, Button} from 'antd';
 
 class MainContent extends React.Component {
 
-
+    
   public render() {
-       
-        return (                       
 
-            <div className="row">
-            <div className="col-md-12 col-sm-12 col-xs-12">
-              <div className="dashboard_graph">
+    const columns = [
+      { title: 'Name', dataIndex: 'name', key: 'name' },     
+      { title: 'Role', dataIndex: 'role', key: 'role' },
+      { title: 'Is Admin', dataIndex: '', key: 'x', render: () => <a href="javascript:;">Set Admin</a> },
+    ];
 
-                <div className="row x_title">
-                  <div className="col-md-3">
-                    <h3>Network Activities <small>Home Page</small></h3>
-                  </div>
-                  <div className="col-md-9">
-                    <div id="reportrange" className="pull-right">
-                      <i className="glyphicon glyphicon-calendar fa fa-calendar"></i>               
-                      <aside className="primary-aside">
-                      <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/users">Users</Link></li>               
-                      </ul>
-                    </aside>
+    const data = [
+      { key: 1, name: 'John Brown', role: 'user', address: 'New York No. 1 Lake Park', description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.' },
+      { key: 2, name: 'Jim Green', role: 'user', address: 'London No. 1 Lake Park', description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.' },
+      { key: 3, name: 'Joe Black', role: 'user', address: 'Sidney No. 1 Lake Park', description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.' },
+    ];
 
-                    </div>
-                  </div>
-                </div>
+        
+    return (   
+         
+       <div className="row">
+       <div className="col-md-12 col-sm-12 col-xs-12">
+         <div className="dashboard_graph">
 
-                <div className="col-md-12 col-sm-12 col-xs-12">                  
-                    <main>                
-                    </main>
-                </div>
-                <div>      
+           <div className="row x_title">
+             <div className="col-md-6">
+               <h3>Network Activities <small>Graph title sub-title</small></h3>
+             </div>
+             <div className="col-md-6">
+
+               <div id="reportrange" className="pull-right">
+                 <i className="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                 <span>December 30, 2014 - January 28, 2015</span> <b className="caret"></b>
+               </div>
+             </div>
+           </div>
+
+
+           <div className="row x_title">
+           <div className="col-md-6">
                       
-                </div>
-                  <div className="clearfix"></div>
-                </div>
-              </div>
-            </div>
-                  
+          
+           </div>
+
+           <div className="col-md-6">
+            <div> <Button type="primary">Reload</Button></div>
+
+              <Table columns={columns} dataSource={data}   />
+
+                    
+           </div>
+         </div>
+         
+           <div className="clearfix"></div>
+
+          </div>
+        </div>
+       </div>
+            
         );
     }
 }
