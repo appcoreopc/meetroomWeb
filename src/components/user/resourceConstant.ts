85fab17480497c9756f7d489b330f39a84b23797
jeremy woo <jeremy.woo@akqa.com>
2018-09-25 07:15:54 +1200

    added api constant external fie.

---
+++ src/components/user/resourceConstant.ts
@@ -0,0 +1,5 @@
+
+export const FETCH_USER_URL : string = 'http://localhost:3000/users/all';
+export const UPDATE_SYSADMIN_URL : string = 'http://localhost:3000/users/setAdmin';
+export const GET_SYSADMIN_URL : string =  'http://localhost:3000/sysadmin/';
+
